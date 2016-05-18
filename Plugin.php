a370f42459d2fc83ef150c2fbe18929626f20767
Volio <volio.liu@gmail.com>
2016-05-18 17:50:12 +0800

    Fix bug ; Add theme setting

--- Plugin.php
+++ Plugin.php
@@ -1,5 +1,6 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
 /**
  * DPlayer for typecho
  *
@@ -8,8 +9,8 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * @version 1.0.0
  * @link http://github.com/volio/DPlayer-for-typecho
  */
-
-class DPlayer_Plugin implements Typecho_Plugin_Interface{
+class DPlayer_Plugin implements Typecho_Plugin_Interface
+{
 
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
@@ -18,11 +19,12 @@ class DPlayer_Plugin implements Typecho_Plugin_Interface{
      * @return void
      * @throws Typecho_Plugin_Exception
      */
-    public static function activate(){
-        Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('DPlayer_Plugin','playerparse');
-        Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('DPlayer_Plugin','playerparse');
-        Typecho_Plugin::factory('Widget_Archive')->header = array('DPlayer_Plugin','playerCss');
-        Typecho_Plugin::factory('Widget_Archive')->footer = array('DPlayer_Plugin','playerJs');
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('DPlayer_Plugin', 'playerparse');
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->excerptEx = array('DPlayer_Plugin', 'playerparse');
+        Typecho_Plugin::factory('Widget_Archive')->header = array('DPlayer_Plugin', 'playerCss');
+        Typecho_Plugin::factory('Widget_Archive')->footer = array('DPlayer_Plugin', 'playerJs');
     }
 
     /**
@@ -33,22 +35,26 @@ class DPlayer_Plugin implements Typecho_Plugin_Interface{
      * @return void
      * @throws Typecho_Plugin_Exception
      */
-    public static function deactivate(){}
+    public static function deactivate()
+    {
+    }
 
     /**
      * 插入顶部css
      */
-    public static function playerCss(){
-        $url = Helper::options()->pluginUrl.'/DPlayer/DPlayer/dist';
-        echo '<link rel="stylesheet" type="text/css" href="'.$url.'/DPlayer.min.css" />
+    public static function playerCss()
+    {
+        $url = Helper::options()->pluginUrl . '/DPlayer/DPlayer/dist';
+        echo '<link rel="stylesheet" type="text/css" href="' . $url . '/DPlayer.min.css" />
         <script>var dPlayers = [];var dPlayerOptions = [];</script>';
     }
 
     /**
      * 插入底部js
      */
-    public static function playerJs(){
-        $url = Helper::options()->pluginUrl.'/DPlayer/DPlayer/dist';
+    public static function playerJs()
+    {
+        $url = Helper::options()->pluginUrl . '/DPlayer/DPlayer/dist';
         echo <<<EOF
 <script type="text/javascript" src="$url/DPlayer.min.js"></script>
 <script>
@@ -73,15 +79,15 @@ EOF;
      * @param string $content
      * @return string
      */
-    public static function playerparse($content,$widget,$lastResult)
+    public static function playerparse($content, $widget, $lastResult)
     {
         $content = empty($lastResult) ? $content : $lastResult;
         if ($widget instanceof Widget_Archive) {
-            if ( false === strpos( $content, '[' ) ) {
+            if (false === strpos($content, '[')) {
                 return $content;
             }
-            $pattern = self::get_shortcode_regex( array('dplayer') );
-            $content = preg_replace_callback("/$pattern/",array('DPlayer_Plugin','parseCallback'), $content);
+            $pattern = self::get_shortcode_regex(array('dplayer'));
+            $content = preg_replace_callback("/$pattern/", array('DPlayer_Plugin', 'parseCallback'), $content);
         }
         return $content;
     }
@@ -103,7 +109,7 @@ EOF;
              * 6 - An extra ] to allow for escaping shortcodes with double [[]]
          */
         // allow [[player]] syntax for escaping the tag
-        if ( $matches[1] == '[' && $matches[6] == ']' ) {
+        if ($matches[1] == '[' && $matches[6] == ']') {
             return substr($matches[0], 1, -1);
         }
         //还原转义后的html
@@ -112,33 +118,35 @@ EOF;
         //[dplayer]标签的属性，类型为array
         $atts = self::shortcode_parse_atts($attr);
         //播放器id
-        $id = md5($_SERVER['HTTP_HOST'].$atts['url']);
+        $id = md5($_SERVER['HTTP_HOST'] . $atts['url']);
 
         $result = array(
-            'url' => $atts['url']?$atts['url']:'',
-            'pic' => $atts['pic']?$atts['pic']:''
+            'url' => $atts['url'] ? $atts['url'] : '',
+            'pic' => $atts['pic'] ? $atts['pic'] : ''
         );
         if (empty($result)) return '';
-        $theme = '#FADFA3';
+        $theme = Typecho_Widget::widget('Widget_Options')->plugin('DPlayer')->maintheme;
+        if (!$theme) $theme = '#FADFA3';
         //播放器默认属性
         $data = array(
-            'id' => $id ,
+            'id' => $id,
             'autoplay' => false,
             'theme' => $theme
         );
         //自动播放
-        $data['autoplay'] = (bool)$data['autoplay'] && $data['autoplay'] !== 'false';
+        $data['autoplay'] = ($atts['autoplay'] == 'true') ? true : false;
+        $data['theme'] = $atts['theme'] ? $atts['autoplay'] : $theme;
         //输出代码
-        $playerCode =  '<div id="player'.$id.'" class="dplayer">';
+        $playerCode = '<div id="player' . $id . '" class="dplayer">';
         $playerCode .= "</div>\n";
         $data['video'] = $result;
         //弹幕部分配置文件
         $danmaku = array(
             'id' => md5($id),
-            'token' => md5(md5($id).date('YmdH',time())),
-            'api'=>'//api.niconico.in/v1/dplayer/danmaku',
+            'token' => md5(md5($id) . date('YmdH', time())),
+            'api' => '//api.niconico.in/v1/dplayer/danmaku',
         );
-        $data['danmaku'] = ($atts['danmu']!='false') ? $danmaku : null;
+        $data['danmaku'] = ($atts['danmu'] != 'false') ? $danmaku : null;
         //加入头部数组
         $js = json_encode($data);
         $playerCode .= <<<EOF
@@ -147,9 +155,17 @@ EOF;
         return $playerCode;
     }
 
-    public static function config(Typecho_Widget_Helper_Form $form){}
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        $maintheme = new Typecho_Widget_Helper_Form_Element_Text(
+            'maintheme', null, '#FADFA3',
+            _t('默认主题颜色'), _t('播放器默认的主题颜色，如 #372e21、#75c、red、blue，该设定会被[dplayer]标签中的theme属性覆盖，默认为 #FADFA3'));
+        $form->addInput($maintheme);
+    }
 
-    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {
+    }
 
     /**
      * Retrieve all attributes from the shortcodes tag.
@@ -167,11 +183,12 @@ EOF;
      *                      Returns empty string if trim( $text ) == ''.
      *                      All other matches are checked for not empty().
      */
-    private static function shortcode_parse_atts($text) {
+    private static function shortcode_parse_atts($text)
+    {
         $atts = array();
         $pattern = '/([\w-]+)\s*=\s*"([^"]*)"(?:\s|$)|([\w-]+)\s*=\s*\'([^\']*)\'(?:\s|$)|([\w-]+)\s*=\s*([^\s\'"]+)(?:\s|$)|"([^"]*)"(?:\s|$)|(\S+)(?:\s|$)/';
         $text = preg_replace("/[\x{00a0}\x{200b}]+/u", " ", $text);
-        if ( preg_match_all($pattern, $text, $match, PREG_SET_ORDER) ) {
+        if (preg_match_all($pattern, $text, $match, PREG_SET_ORDER)) {
             foreach ($match as $m) {
                 if (!empty($m[1]))
                     $atts[strtolower($m[1])] = stripcslashes($m[2]);
@@ -186,9 +203,9 @@ EOF;
             }
 
             // Reject any unclosed HTML elements
-            foreach( $atts as &$value ) {
-                if ( false !== strpos( $value, '<' ) ) {
-                    if ( 1 !== preg_match( '/^[^<]*+(?:<[^>]*+>[^<]*+)*+$/', $value ) ) {
+            foreach ($atts as &$value) {
+                if (false !== strpos($value, '<')) {
+                    if (1 !== preg_match('/^[^<]*+(?:<[^>]*+>[^<]*+)*+$/', $value)) {
                         $value = '';
                     }
                 }
@@ -221,8 +238,9 @@ EOF;
      * @param array $tagnames List of shortcodes to find. Optional. Defaults to all registered shortcodes.
      * @return string The shortcode search regular expression
      */
-    private static function get_shortcode_regex( $tagnames = null ) {
-        $tagregexp = join( '|', array_map('preg_quote', $tagnames) );
+    private static function get_shortcode_regex($tagnames = null)
+    {
+        $tagregexp = join('|', array_map('preg_quote', $tagnames));
 
         // WARNING! Do not change this regex without changing do_shortcode_tag() and strip_shortcode_tag()
         // Also, see shortcode_unautop() and shortcode.js.
@@ -232,27 +250,27 @@ EOF;
             . "($tagregexp)"                     // 2: Shortcode name
             . '(?![\\w-])'                       // Not followed by word character or hyphen
             . '('                                // 3: Unroll the loop: Inside the opening shortcode tag
-            .     '[^\\]\\/]*'                   // Not a closing bracket or forward slash
-            .     '(?:'
-            .         '\\/(?!\\])'               // A forward slash not followed by a closing bracket
-            .         '[^\\]\\/]*'               // Not a closing bracket or forward slash
-            .     ')*?'
+            . '[^\\]\\/]*'                   // Not a closing bracket or forward slash
+            . '(?:'
+            . '\\/(?!\\])'               // A forward slash not followed by a closing bracket
+            . '[^\\]\\/]*'               // Not a closing bracket or forward slash
+            . ')*?'
             . ')'
             . '(?:'
-            .     '(\\/)'                        // 4: Self closing tag ...
-            .     '\\]'                          // ... and closing bracket
+            . '(\\/)'                        // 4: Self closing tag ...
+            . '\\]'                          // ... and closing bracket
             . '|'
-            .     '\\]'                          // Closing bracket
-            .     '(?:'
-            .         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
-            .             '[^\\[]*+'             // Not an opening bracket
-            .             '(?:'
+            . '\\]'                          // Closing bracket
+            . '(?:'
+            . '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags
+            . '[^\\[]*+'             // Not an opening bracket
+            . '(?:'
             . '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag
-            .                 '[^\\[]*+'         // Not an opening bracket
-            .             ')*+'
-            .         ')'
-            .         '\\[\\/\\2\\]'             // Closing shortcode tag
-            .     ')?'
+            . '[^\\[]*+'         // Not an opening bracket
+            . ')*+'
+            . ')'
+            . '\\[\\/\\2\\]'             // Closing shortcode tag
+            . ')?'
             . ')'
             . '(\\]?)';                          // 6: Optional second closing brocket for escaping shortcodes: [[tag]]
     }
